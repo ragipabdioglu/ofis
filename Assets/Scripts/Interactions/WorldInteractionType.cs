98285fa54bd45a856dfbc85ee76d4a3ee17f33a7
Ragıp Abdioğlu <73760342+ragipabdioglu@users.noreply.github.com>
2026-06-09 22:58:26 +0300

    Add world interaction type enum

---
+++ Assets/Scripts/Interactions/WorldInteractionType.cs
@@ -0,0 +1,14 @@
+namespace OFIS.Interactions
+{
+    public enum WorldInteractionType
+    {
+        None = 0,
+        CorpseInspectOrCarry = 10,
+        MeetingJoin = 20,
+        SabotageRepair = 30,
+        Task = 40,
+        Sabotage = 50,
+        VictimNote = 60,
+        DoorPanel = 70
+    }
+}
