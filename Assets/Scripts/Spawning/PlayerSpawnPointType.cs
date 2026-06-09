b1621f8a3d2fb3a17ba9965f1cd99b3b2f3cf2dc
Ragıp Abdioğlu <73760342+ragipabdioglu@users.noreply.github.com>
2026-06-09 23:44:38 +0300

    Add player spawn point type enum

---
+++ Assets/Scripts/Spawning/PlayerSpawnPointType.cs
@@ -0,0 +1,12 @@
+namespace OFIS.Spawning
+{
+    public enum PlayerSpawnPointType
+    {
+        None = 0,
+        Default = 1,
+        Lobby = 2,
+        Office = 3,
+        MeetingRoom = 4,
+        Spectator = 5
+    }
+}
