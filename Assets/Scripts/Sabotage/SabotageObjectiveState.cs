7e896a0aa5c5662b0236a22bdfa7b5530a4161cd
Ragıp Abdioğlu <73760342+ragipabdioglu@users.noreply.github.com>
2026-06-11 00:37:21 +0300

    Add sabotage objective state enum

---
+++ Assets/Scripts/Sabotage/SabotageObjectiveState.cs
@@ -0,0 +1,12 @@
+namespace OFIS.Sabotage
+{
+    public enum SabotageObjectiveState
+    {
+        None = 0,
+        Inactive = 1,
+        Active = 2,
+        Repairing = 3,
+        Repaired = 4,
+        Expired = 5
+    }
+}
