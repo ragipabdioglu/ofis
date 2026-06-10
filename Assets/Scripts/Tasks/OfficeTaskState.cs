08bdd2f2919cb01f47c466c2d56160a288e3be3f
Ragıp Abdioğlu <73760342+ragipabdioglu@users.noreply.github.com>
2026-06-10 22:55:24 +0300

    Add office task state enum

---
+++ Assets/Scripts/Tasks/OfficeTaskState.cs
@@ -0,0 +1,11 @@
+namespace OFIS.Tasks
+{
+    public enum OfficeTaskState
+    {
+        None = 0,
+        Available = 1,
+        InProgress = 2,
+        Completed = 3,
+        Blocked = 4
+    }
+}
