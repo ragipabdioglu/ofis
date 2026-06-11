a48c825660e5dd89db39c101ae5fb6e64a5347b2
Ragıp Abdioğlu <73760342+ragipabdioglu@users.noreply.github.com>
2026-06-11 19:33:16 +0300

    Add deduction outcome type

---
+++ Assets/Scripts/Meetings/DeductionOutcomeType.cs
@@ -0,0 +1,12 @@
+namespace OFIS.Meetings
+{
+    public enum DeductionOutcomeType
+    {
+        None = 0,
+        NoVotes = 1,
+        Tie = 2,
+        CorrectAccusation = 3,
+        WrongAccusation = 4,
+        InvalidTarget = 5
+    }
+}
