93115c32cdd27e116f0cc368a77bf7c5400443e1
Ragıp Abdioğlu <73760342+ragipabdioglu@users.noreply.github.com>
2026-06-11 03:12:27 +0300

    Add meeting report type enum

---
+++ Assets/Scripts/Meetings/MeetingReportType.cs
@@ -0,0 +1,14 @@
+namespace OFIS.Meetings
+{
+    public enum MeetingReportType
+    {
+        None = 0,
+        Suspicion = 1,
+        Defense = 2,
+        LocationClaim = 3,
+        TaskProgress = 4,
+        SabotageStatus = 5,
+        CorpseReport = 6,
+        GeneralNote = 7
+    }
+}
